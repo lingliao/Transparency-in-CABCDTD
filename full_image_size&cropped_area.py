c6c2288a8fc27563accd7bb9a621907d79c4d6d9
lingliao <91222367+lingliao@users.noreply.github.com>
2024-04-21 01:53:30 -0500

    full_image_size&cropped_area.py

--- full_image_size&cropped_area.py
+++ full_image_size&cropped_area.py
@@ -15,20 +15,19 @@ import numpy as np
 # Calculate the cropped area per 598 by 598 pixels #
 ####################################################
 
-# Define the function
+# For non-zero area calculation
+# Define the function for testing images
 def process_image_and_extract_groups(image_path):
     # Define the regex pattern
-    pattern = r'Mass-Training_P_(\d+)_(\w+)_(\w+)_' #for training dataset, make sure you pick the proper one
-    pattern = r'Mass-Test_P_(\d+)_(\w+)_(\w+)_' #for training dataset, make sure you pick the proper one
-
-
+    pattern = r'Mass-Test_P_(\d+)_(\w+)_(\w+)_'  # Fro testing image
+    # pattern = r'Mass-Training_P_(\d+)_(\w+)_(\w+)_' # For training image
     match = re.search(pattern, image_path)
     if match:
         number = match.group(1)
         left = match.group(2)
         cc = match.group(3)
-        result = f'Mass-Test_P_{number}_{left}_{cc}'
-
+        result = f'Mass-Test_P_{number}_{left}_{cc}' # Update this part accordingly
+        # result = f'Mass-Training_P_{number}_{left}_{cc}' # Update this part accordingly
         # Open the image
         image = Image.open(image_path)
 
@@ -45,26 +44,74 @@ def process_image_and_extract_groups(image_path):
         print(f"No match found for file: {image_path}")
         return None, None, None
 
-# Create an empty DataFrame
-df_test = pd.DataFrame(columns=['name', 'file_path', 'area_percentage'])
+# Create an empty DataFrame, df for training, df_test for testing image, respectively
+df_test = pd.DataFrame(columns=['name', 'file_path', 'area_percentage']) # Define as df for training images
 
 # Replace 'path_to_folder' with the actual path to your folder containing images
-folder_path = '/content/test_598_same'
+folder_path = '/content/all_598'
 
 for filename in os.listdir(folder_path):
     if filename.endswith(".png"):  # Check only PNG files, adjust if needed
         image_path = os.path.join(folder_path, filename)
         results, image, nonzero_area_percentage = process_image_and_extract_groups(image_path)
         if results and image and nonzero_area_percentage:
-            df_test = df_test.append({'name': results, 'file_path': image_path, 'area_percentage': nonzero_area_percentage}, ignore_index=True)
+            df_test = pd.concat([df_test, pd.DataFrame({'name': [results], 'file_path': [image_path], 'area_percentage': [nonzero_area_percentage]})], ignore_index=True) # Replace df_test with df fortraining images
 
-print("Calculated!")
+# Save the df_test
+# df_test.to_csv('output.csv', index=False)
+print("Done for testing images")
+
+# Define the function for training images
+def process_image_and_extract_groups(image_path):
+    # Define the regex pattern
+    # pattern = r'Mass-Test_P_(\d+)_(\w+)_(\w+)_'  # Fro testing image
+    pattern = r'Mass-Training_P_(\d+)_(\w+)_(\w+)_' # For training image
+    match = re.search(pattern, image_path)
+    if match:
+        number = match.group(1)
+        left = match.group(2)
+        cc = match.group(3)
+        # result = f'Mass-Test_P_{number}_{left}_{cc}' # Update this part accordingly
+        result = f'Mass-Training_P_{number}_{left}_{cc}' # Update this part accordingly
+        # Open the image
+        image = Image.open(image_path)
+
+        # Convert the grayscale image to a NumPy array
+        image_array = np.array(image)
+
+        # Calculate the non-zero area
+        nonzero_pixels = np.count_nonzero(image_array)
+        total_pixels = image_array.size
+        nonzero_area_percentage = (nonzero_pixels / total_pixels) * 100
+
+        return result, image, nonzero_area_percentage
+    else:
+        print(f"No match found for file: {image_path}")
+        return None, None, None
+
+# Create an empty DataFrame, df for training, df_test for testing image, respectively
+df = pd.DataFrame(columns=['name', 'file_path', 'area_percentage']) # Define as df for training images
+
+# Replace 'path_to_folder' with the actual path to your folder containing images
+folder_path = '/content/all_598'
+
+for filename in os.listdir(folder_path):
+    if filename.endswith(".png"):  # Check only PNG files, adjust if needed
+        image_path = os.path.join(folder_path, filename)
+        results, image, nonzero_area_percentage = process_image_and_extract_groups(image_path)
+        if results and image and nonzero_area_percentage:
+            df = pd.concat([df, pd.DataFrame({'name': [results], 'file_path': [image_path], 'area_percentage': [nonzero_area_percentage]})], ignore_index=True) # Replace df_test with df fortraining images
+
+# Save the df
+# df.to_csv('output.csv', index=False)
+print("Done for trainging images")
 
 #####################################################
 # Merge the area info in both train and test images #
 # Add the pathology information as well             #
 #####################################################
 
+# Make sure you generated both df and df_test
 # Merge the DataFrames using pd.concat()
 merged_df = pd.concat([df, df_test]) #df and df_test contain area info for train and test cropped images, respectively.
 sorted_merged_df = merged_df.sort_values(by='name')
@@ -88,7 +135,7 @@ def copy_pathology(row):
 sorted_merged_df['pathology'] = sorted_merged_df.apply(copy_pathology, axis=1)
 
 # Save the output files
-sorted_merged_df.to_csv('/content/content/598_percentage_all.csv')
+sorted_merged_df.to_csv('/content/598_percentage_all.csv')
 
 #################################
 # Calculate the full image size #
@@ -141,4 +188,4 @@ df['pathology'] = df.apply(copy_pathology, axis=1)
 df_sort = df.sort_values(by='Subject_ID')
 
 #save the output
-df_sort.to_csv('/content/metadata/heaght_width_FULL.csv')
+df_sort.to_csv('/content/heaght_width_FULL.csv')
